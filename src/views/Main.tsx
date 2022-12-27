8ccc968a7d6bcc05ff216832aa3f8e328b9ffe5a
Dagore <david.gomez.redondo.dev@gmail.com>
2022-12-27 21:11:06 +0100

    h1

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -4,6 +4,7 @@ import PrimaryLayout from '../layouts/PrimaryLayout'
 export default function Main (): JSX.Element {
   return (
     <PrimaryLayout>
+      <h1 className='text-4xl font-bold text-center text-fuchsia-500'>Juego de memoria</h1>
       <SelectMode modes={['Fácil', 'Normal', 'Difícil']} />
     </PrimaryLayout>
   )
